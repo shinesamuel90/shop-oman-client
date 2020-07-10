12706321077af6ff89b1c536b1273c45fcea9a08
shinesamuel90 <shinesamuel90@gmail.com>
2020-07-10 18:08:49 +0400

    Project layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,27 @@ import { CommonLayoutComponent } from './common/common-layout.component';
 import { AuthenticationLayoutComponent } from './common/authentication-layout.component';
 
 // Routing Module
-import { AppRoutes } from './app.routing';
+//import { AppRoutes, AppRoutingModule } from './app.routing';
 
 // App Component
 import { AppComponent } from './app.component';
 import { AppCustomLayoutComponent } from './layout/app-custom-layout/app-custom-layout.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
+import { HomeHeaderComponent } from './layout/home-header/home-header.component';
+import { HomeFooterComponent } from './layout/home-footer/home-footer.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { AdminHeaderComponent } from './layout/admin-header/admin-header.component';
+import { AdminFooterComponent } from './layout/admin-footer/admin-footer.component';
+import { AdminNavComponent } from './layout/admin-nav/admin-nav.component';
+import { HomeNavComponent } from './layout/home-nav/home-nav.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
     imports: [
         BrowserModule,
-        RouterModule.forRoot(AppRoutes, { useHash: true }),
+        AppRoutingModule,
         SharedModule,
         TemplateModule
     ],
@@ -31,7 +40,15 @@ import { FooterComponent } from './layout/footer/footer.component';
         AuthenticationLayoutComponent,
         AppCustomLayoutComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        HomeLayoutComponent,
+        HomeHeaderComponent,
+        HomeFooterComponent,
+        AdminLayoutComponent,
+        AdminHeaderComponent,
+        AdminFooterComponent,
+        AdminNavComponent,
+        HomeNavComponent
     ],
     providers: [TemplateService],
     bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 // Layouts
 import { CommonLayoutComponent } from './common/common-layout.component';
 import { AuthenticationLayoutComponent } from './common/authentication-layout.component';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
+import { NgModule } from '@angular/core';
 
-export const AppRoutes: Routes = [
+ const AppRoutes: Routes = [
     {
         path: '',
         redirectTo: 'dashboard',
@@ -30,5 +32,23 @@ export const AppRoutes: Routes = [
             }
         ]
     }
+//    {
+//         path: '',
+//         redirectTo: '/home',
+//         pathMatch: 'full'
+//       },
+//       { 
+//         path: 'home',
+//         component: HomeLayoutComponent,
+//         loadChildren: () =>
+//           import('./modules/home/home.module').then(m => m.HomeModule)
+//       },
+//       // Fallback when no prior routes is matched
+//       { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
 
+@NgModule({
+    imports: [RouterModule.forRoot(AppRoutes,{ useHash: true })],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
