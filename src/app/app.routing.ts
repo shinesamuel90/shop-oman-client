9da9b11034fbf76a2c7569b0c8b485e8758019d6
shinesamuel90 <shinesamuel90@gmail.com>
2020-07-11 18:37:48 +0400

    Admin routing added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,50 +5,38 @@ import { CommonLayoutComponent } from './common/common-layout.component';
 import { AuthenticationLayoutComponent } from './common/authentication-layout.component';
 import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
 import { NgModule } from '@angular/core';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { AdminModule } from './modules/admin/admin.module';
 
  const AppRoutes: Routes = [
-    // {
-    //     path: '',
-    //     redirectTo: 'dashboard',
-    //     pathMatch: 'full',
-    // },
-    // {
-    //     path: '',
-    //     component: CommonLayoutComponent,
-    //     children: [
-    //         {
-    //             path: 'dashboard',
-    //             loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-    //         }
-    //     ]
-    // },
-    // {
-    //     path: '',
-    //     component: AuthenticationLayoutComponent,
-    //     children: [
-    //         {
-    //             path: 'authentication',
-    //             loadChildren: () => import('./extras/authentication.modules').then(m => m.AuthenticationModule)
-    //         }
-    //     ]
-    // }
+   
    {
         path: '',
-        redirectTo: '/home',
+        redirectTo: '/home/main',
         pathMatch: 'full'
       },
-      { 
-        path: 'home',
-        component: HomeLayoutComponent,
-        loadChildren: () =>
-          import('./modules/home/home.module').then(m => m.HomeModule)
-      },
+    //   {
+    // /   path:'admin',
+    // //    redirectTo:'/admin/dashboard',
+    // //    pathMatch:'full'
+    // //  },
+      {
+        path:'admin',
+    component:AdminLayoutComponent,
+    loadChildren:()=>import('./modules/admin/admin.module').then(m=>AdminModule)
+    },
+    { 
+      path: 'home',
+      component: HomeLayoutComponent,
+      loadChildren: () =>
+        import('./modules/home/home.module').then(m => m.HomeModule)
+    },
       // Fallback when no prior routes is matched
       { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(AppRoutes,{ useHash: true })],
+    imports: [RouterModule.forRoot(AppRoutes)],
     exports: [RouterModule]
   })
   export class AppRoutingModule { }
---
+++ src/app/modules/admin/admin-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/admin/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
