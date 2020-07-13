cea09984c045c3422b9539e9819de1a6386bd1a1
shinesamuel90 <shinesamuel90@gmail.com>
2020-07-13 22:30:37 +0400

    added product component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { MainComponent } from './page/main/main.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, ProductsComponent],
   imports: [
     CommonModule,
     
