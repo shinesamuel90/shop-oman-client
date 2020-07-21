60ce545ba9df07e138bed7a57d2c65a85b8bbc07
Vinay Valsan <vinayvvv444@gmail.com>
2020-07-21 20:19:07 +0400

    added admin http services

--- src/app/shared/services/admin.service.ts
+++ src/app/shared/services/admin.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  readonly BaseURI = 'http://localhost:57361/api';
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
+    FullName: [''],
+    Passwords : this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, {validator: this.comparePasswords})
+
+  });
+
+  comparePasswords(fb: FormGroup) {
+    const confirmPswrdCtrl = fb.get('ConfirmPassword');
+    // PasswordMismatch
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      // tslint:disable-next-line: triple-equals
+      if (fb.get('Password').value != confirmPswrdCtrl.value) {
+      confirmPswrdCtrl.setErrors({passwordMismatch: true});
+      } else {
+      confirmPswrdCtrl.setErrors(null);
+      }
+    }
+  }
+
 
   roleMatch(allowedRoles: any[]): boolean {
     let isMatch = false;
@@ -20,4 +49,24 @@ export class AdminService {
     });
     return isMatch;
   }
+
+  register() {
+    const body = {
+      UserName: this.formModel.value.UserName,
+      Email: this.formModel.value.Email,
+      FullName: this.formModel.value.FullName,
+      Password: this.formModel.value.Passwords.Password
+    };
+    return this.http.post(this.BaseURI + '/ApplicationUser/Register', body);
+      }
+
+      login(formData) {
+        return this.http.post(this.BaseURI + '/ApplicationUser/Login', formData);
+      }
+
+      getUserProfile() {
+        // var tokenHeader =  new HttpHeaders({'Authorization':'Bearer'+localStorage.getItem('token')});
+       return this.http.get(this.BaseURI + '/UserProfile');
+
+      }
 }
